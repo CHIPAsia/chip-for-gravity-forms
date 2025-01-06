b77cd245aca42c2830023e80583c4fc552d6d88f
Wan Zulkarnain <wanzulkarnain69@gmail.com>
2025-01-06 21:21:27 +0800

    Fix code style to WordPress

--- chip-for-gravity-forms.php
+++ chip-for-gravity-forms.php
@@ -17,32 +17,32 @@
 
 defined( 'ABSPATH' ) || die();
 
-define( 'GF_CHIP_MODULE_VERSION', 'v1.0.7');
+define( 'GF_CHIP_MODULE_VERSION', 'v1.0.7' );
 define( 'GF_CHIP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 
 add_action( 'gform_loaded', array( 'GF_CHIP_Bootstrap', 'load_addon' ), 5 );
 
 class GF_CHIP_Bootstrap {
 
-  public static function load_addon() {
+	public static function load_addon() {
 
-    require_once GF_CHIP_PLUGIN_PATH . '/api.php';
-    require_once GF_CHIP_PLUGIN_PATH . '/class-gf-chip.php';
+		require_once GF_CHIP_PLUGIN_PATH . '/api.php';
+		require_once GF_CHIP_PLUGIN_PATH . '/class-gf-chip.php';
 
-    GFAddOn::register( 'GF_Chip' );
+		GFAddOn::register( 'GF_Chip' );
 
-    add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), array('GF_CHIP_Bootstrap','gf_chip_setting_link'));
-    
-  }
+		add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( 'GF_CHIP_Bootstrap', 'gf_chip_setting_link' ) );
 
-  public static function gf_chip_setting_link($links) {
-    $new_links = array(
-      'settings' => sprintf(
-        '<a href="%1$s">%2$s</a>', admin_url('admin.php?page=gf_settings&subview=gravityformschip'), esc_html__('Settings', 'gravityformschip')
-      )
-    );
+	}
 
-    return array_merge($new_links, $links);
-  }
+	public static function gf_chip_setting_link( $links ) {
+		$new_links = array(
+			'settings' => sprintf(
+				'<a href="%1$s">%2$s</a>', admin_url( 'admin.php?page=gf_settings&subview=gravityformschip' ), esc_html__( 'Settings', 'gravityformschip' )
+			)
+		);
+
+		return array_merge( $new_links, $links );
+	}
 
 }
